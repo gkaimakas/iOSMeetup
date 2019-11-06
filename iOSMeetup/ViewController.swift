18f124823349f0c628b3d9fb97dd65bc654c5a3d
George Kaimakas <gkaimakas@gmail.com>
2019-11-06 19:48:12 +0200

    Initial Commit

---
+++ iOSMeetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSMeetup
+//
+//  Created by George Kaimakas on 6/11/19.
+//  Copyright © 2019 George Kaimakas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
